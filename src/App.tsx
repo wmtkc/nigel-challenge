577791d1fc143efbb845d7defc99235c0b4df773
William Casey <will.casey5@gmail.com>
2022-04-24 13:11:02 -0600

    fix same-list reorder bug

--- src/App.tsx
+++ src/App.tsx
@@ -6,45 +6,51 @@ import { CardMap } from './types';
 
 function App() {
   const [cards, setCards] = useState<CardMap>({
-    toDo: ['example card'],
-    inProgress: ['example 2'],
+    toDo: ['example 1', 'example 2', 'example 3'],
+    inProgress: [],
     done: []
   })
 
   // Reorder logic adapted from example at https://github.com/atlassian/react-beautiful-dnd
-  const reorderCards = (destination: DraggableLocation, source: DraggableLocation) => {
-    const current = [...cards[source.droppableId]];
-    const next = [...cards[destination.droppableId]];
-    const target = current[source.index];
+  const reorderCards = (source: DraggableLocation, destination?: DraggableLocation) => {
+    // only reorder if the destination is resolved
+    if (destination) {
+      const current = [...cards[source.droppableId]];
+      const next = [...cards[destination.droppableId]];
+      const target = current[source.index];
 
-    // moving to same list
-    if (source.droppableId === destination.droppableId) {
-      const reordered = Array.from(current);
-      const [removed] = reordered.splice(source.index, 1);
-      reordered.splice(destination.index, 0, removed);
+      // moving to same list
+      if (source.droppableId === destination.droppableId) {
+        const reordered = Array.from(current);
 
-      return {
-        ...cards,
-        [source.droppableId]: reordered
-      };
+        // remove at old index
+        const [removed] = reordered.splice(source.index, 1);
+        // insert at new index
+        reordered.splice(destination.index, 0, removed);
+
+        setCards({
+          ...cards,
+          [source.droppableId]: reordered
+        });
+      } else {
+        // moving to different list
+
+        // remove from original
+        current.splice(source.index, 1);
+        // insert into next
+        next.splice(destination.index, 0, target);
+
+        setCards({
+          ...cards,
+          [source.droppableId]: current,
+          [destination.droppableId]: next
+        });
+      }
     }
-
-    // moving to different list
-
-    // remove from original
-    current.splice(source.index, 1);
-    // insert into next
-    next.splice(destination.index, 0, target);
-
-    setCards({
-      ...cards,
-      [source.droppableId]: current,
-      [destination.droppableId]: next
-    });
   }
 
   return (
-    <DragDropContext onDragEnd={({destination, source}) => reorderCards(destination!, source)}>
+    <DragDropContext onDragEnd={({source, destination}) => reorderCards(source, destination)}>
       <main>
         {
           Object.entries(cards).map(([key, value]) => (
